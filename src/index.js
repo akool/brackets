07119d44dc533d6a54b6cfe4d3402e6fe626180e
akool <akoolny@gmail.com>
2021-02-18 23:39:48 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let flag = false;
+
+  while (str) {
+    flag = false;
+
+    for (let brackets of bracketsConfig) {
+      const pair = brackets[0] + brackets[1];
+
+      if (str.includes(pair)) {
+        str = str.replace(pair, '');
+        flag = true;
+      }
+    }
+    
+    if (!flag) break;
+  }
+
+  return !str.length;
 }
